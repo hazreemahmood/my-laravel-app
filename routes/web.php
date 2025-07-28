ede52daff42c1a74eacaff240f836acfb174389d
Hazree Mahmood <hazreemahmood@gmail.com>
2025-07-29 02:26:14 +0800

    modified route

--- routes/web.php
+++ routes/web.php
@@ -45,10 +45,12 @@ Route::group(['middleware' => 'auth'], function () {
 	Route::get('/profile-static', [PageController::class, 'profile'])->name('profile-static'); 
 	Route::get('/sign-in-static', [PageController::class, 'signin'])->name('sign-in-static');
 	Route::get('/sign-up-static', [PageController::class, 'signup'])->name('sign-up-static'); 
-	Route::get('/{page}', [PageController::class, 'index'])->name('page');
-	Route::post('logout', [LoginController::class, 'logout'])->name('logout');// routes/web.php
+	
 	Route::get('/chat', [ChatController::class, 'index']);
 	Route::post('/upload', [ChatController::class, 'upload']);
 	Route::get('/fetchmessage', [ChatController::class, 'fetchMessages']);
+
+	Route::get('/{page}', [PageController::class, 'index'])->name('page');
+	Route::post('logout', [LoginController::class, 'logout'])->name('logout');// routes/web.php
 
 });
